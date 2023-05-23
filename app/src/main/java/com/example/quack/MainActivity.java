ea216767e9e55052b1a6bb75bdd5ba87a61eb34f
200910034 <flawcoaser@gmail.com>
2023-05-23 13:57:00 -0700

    added sounds

--- app/src/main/java/com/example/quack/MainActivity.java
+++ app/src/main/java/com/example/quack/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.quack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnChicken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
